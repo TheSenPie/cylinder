6220b254d997b1f0a9c66f90473f78c96a21e34b
SenPie <smbo.voskanyan2002@gmail.com>
2022-11-17 02:56:27 +0400

    linux fix

--- src/util/camera.c
+++ src/util/camera.c
@@ -11,8 +11,8 @@ void perspective_camera_init(struct PerspectiveCamera *self, f32 fov) {
     self->zfar = 1000.0f;
 
     self->ro = 6;
-    self->theta = M_PI_4;
-    self->fi = M_PI_4;
+    self->theta = GLM_PI_4;
+    self->fi = GLM_PI_4;
     perspective_camera_look_at(self, glms_vec3_zero());
 }
 
